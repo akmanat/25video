804171e8860e30654c65286c2dcd20d16b8597d4
akmanat <sadykova.akmanat@gmail.com>
2021-06-21 16:59:26 +0600

    try to commit

---
+++ src/main/java/kz/akmanat/springcourse/config/config/SecondController.java
@@ -0,0 +1,13 @@
+package kz.akmanat.springcourse.config.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecondController {
+    @GetMapping("/exit")
+    public String exit(){
+        return "second/exit";
+
+    }
+}
